2728c17612fa8d6131fedce14fd80ca0a50f8300
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-10-12 23:40:47 -0600

    conectando el modelo a sequelize ya guardado

--- src/database.ts
+++ src/database.ts
@@ -1,2 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
 import { Tarea } from "./models/tareas";
+
+export const sequelize = new Sequelize({
+    database: "tareas",
+    dialect: "mysql",
+    username: "root", //vamos a agregar las variables de entorno que mas adelante agregaremos 
+    password: "root",
+    models: [Tarea]
+});
