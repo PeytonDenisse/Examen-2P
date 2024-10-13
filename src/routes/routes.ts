74775a79cd48aa415c5c2c517c03b3c1fe8c09d4
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-10-13 00:17:50 -0600

    delete tarea, con guardado

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -26,3 +26,55 @@ router.get("/", async (req: Request, res: Response) => {
     }
 });
 
+// obtener una tarea por id
+router.get("/:id", async (req: Request, res: Response) => {
+    try {
+        const tarea = await Tarea.findByPk(req.params.id);
+        if (tarea) {
+             res.json(tarea);
+        }else{
+            res.status(404).json({message: "Tarea no encontrada"});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+        
+    }});
+
+// actualizar una tarea
+router.put("/:id", async (req: Request, res: Response) => {
+    try{
+        const tarea = await Tarea.findByPk(req.params.id);
+        if (tarea) {
+            tarea.title = req.body.title || tarea.title;
+            tarea.description = req.body.description || tarea.description;
+            tarea.completed = req.body.completed || tarea.completed;
+            await tarea.save();
+            res.json(tarea);
+        }else{
+            return res.status(404).json({message: "Tarea no encontrada"});
+        }
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});   
+    }});
+
+
+// eliminar una tarea
+router.delete("/:id", async (req: Request, res: Response)=>{
+    try {
+        const tarea = await Tarea.findByPk(req.params.id);
+        if (tarea) {
+            await tarea.destroy();
+            res.json({message: "Tarea eliminada"});
+        }else{
+            res.status(404).json({message: "Tarea no encontrada"});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+        
+    }
+})
+
+
