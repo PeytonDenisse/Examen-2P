6ba29c17ef3986f494d4ce604f2e83c7a230cf51
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-10-13 00:56:47 -0600

    modificamos nuestro database para aplicar las variables de entorno

--- src/database.ts
+++ src/database.ts
@@ -1,21 +1,17 @@
 import { Sequelize } from "sequelize-typescript";
 import { Tarea } from "./models/tareas";
+import dotenv from "dotenv";
+
+// Cargar variables de entorno
+dotenv.config();
 
 export const sequelize = new Sequelize({
-    database: "tareas",
-    dialect: "mysql",
-    username: "root", //vamos a agregar las variables de entorno que mas adelante agregaremos 
-    password: "root",
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT as any,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     models: [Tarea]
 });
 
-export const dbConnection = async () =>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-        await sequelize.sync({force: true});
-        console.log('Database synchronized');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+// Resto del código de dbConnection...
